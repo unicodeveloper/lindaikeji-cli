3a52a98e4737e6f63487e70b38886d11ac8c30fb
Oladipo Fasoro <dfasoro@gmail.com>
2016-05-27 11:08:18 +0100

    Reading a URL now includes the comments.

--- factory/reader.js
+++ factory/reader.js
@@ -22,8 +22,43 @@ function parseContent(html) {
   var $ = cheerio.load(html),
       articleMarkup = $('.post-outer div.post.hentry').html(),
       content = getTextFromHtml(articleMarkup);
-
-  return content;
+      
+      //@dfasoro added this.
+      var comments = [];
+      var commentsNode = $('#comments');
+      comments.push($('H4', commentsNode).first().text());
+      
+      var authors = [], body = [], time = [];
+      
+      try {
+          $('#comments-block DT.comment-author', commentsNode).each(function () {
+              var author_line = $(this).text().trim();
+              author_line = author_line.substr(0, author_line.length - 7).trim();
+              authors.push(author_line);
+          });
+          $('#comments-block DD.comment-body', commentsNode).each(function () {
+              var body_line = getTextFromHtml($(this).html()).trim();
+              body.push(body_line);
+          });
+          $('#comments-block DD.comment-footer SPAN.comment-timestamp > A', commentsNode).each(function () {
+              var time_line = $(this).text().trim();
+              time.push(time_line);
+          });
+          //console.log(authors);
+          //console.log(body);
+          //console.log(time);
+      }
+      catch (pex) {
+          //any yawa that gas should not make the original post not to show up right.
+      }
+      
+      authors.forEach(function (value, index, array) {
+          
+          var text = `${index + 1}) ${authors[index]} @ ${time[index]} said:\n${body[index]}\n`;
+          comments.push(text);
+      });
+      
+  return content + "\n\n" + comments.join("\n");
 }
 
 function requestContent(url) {
@@ -35,7 +70,7 @@ function showContent(url) {
   requestContent(url).then(function (body) {
     var content = parseContent(body);
     printArticle(content);
-  }, function () {
+  }, function (e) {
     console.log('Oops! Something went wrong!');
   });
 }
